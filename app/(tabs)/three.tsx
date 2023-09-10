e66289c3a0e8060a7442d27ce94595cf2b75cb5b
ygqygq2 <ygqygq2@qq.com>
2023-09-10 23:59:22 +0800

    feat: Image 组件学习
    Co-authored-by: ygqygq2 <29ygq@sina.com>

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -1,10 +1,12 @@
 import React, {FC, useEffect, useState} from 'react';
 import {withExpoSnack} from 'nativewind';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import {styled} from 'nativewind';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
+
+const imageUri = 'https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png';
 
 const Demo: FC = () => {
   const [count, setCount] = useState(0);
@@ -22,6 +24,7 @@ const Demo: FC = () => {
   return (
     <StyledView>
       <StyledText className="text-red-600">{count}</StyledText>
+      <Image source={{uri: imageUri}} blurRadius={3} />
     </StyledView>
   );
 };
